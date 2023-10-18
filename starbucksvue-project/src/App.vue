4e928ffe5154fc9e83d7399cced6ddeeb85c6560
shinoda <s.saya2x4@gmail.com>
2023-10-18 18:56:25 +0900

    Add content.vue.

--- starbucksvue-project/src/App.vue
+++ starbucksvue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import{ ref } from 'vue'
   import Header from './header.vue'
+  import Content from './content.vue'
   import Info from './info.vue'
   import Footer from './footer.vue'
 </script>
@@ -8,6 +9,7 @@
 <template>
   <div class="container">
     <Header />
+    <Content />
     <Info />
     <Footer />
   </div>
