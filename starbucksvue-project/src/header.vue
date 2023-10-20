63a651e3bf04d275912fba94a1daf0446b8413ee
shinoda <s.saya2x4@gmail.com>
2023-10-20 14:25:38 +0900

    add. URL setting in text.

--- starbucksvue-project/src/header.vue
+++ starbucksvue-project/src/header.vue
@@ -12,16 +12,22 @@
     <div class="header">
     <!-- Logo,MENU,REWARDS,GIFT CARDS -->
     <div class="header-left">
-        <img src="@/assets/logo.png" alt="Logo" class="header_logo">
-        <div class="header_leftItem">{{ header_menu }}</div>
-        <div class="header_leftItem">{{ header_rewards }}</div>
-        <div class="header_leftItem">{{ header_giftcards }}</div>
+        <a href="https://www.starbucks.com/"><img src="@/assets/logo.png" alt="Logo" class="header_logo"></a>
+        <a href="https://www.starbucks.com/menu">
+        <div class="header_leftItem">{{ header_menu }}</div></a>
+        <a href="https://www.starbucks.com/rewards">
+        <div class="header_leftItem">{{ header_rewards }}</div></a>
+        <a href="https://www.starbucks.com/gift">
+        <div class="header_leftItem">{{ header_giftcards }}</div></a>
         </div>
     <!-- Find a store,Sign in,join now -->
     <div class="header-right">
-        <div class="header_rightItem">{{ header_findastore }}</div>
-        <div class="header_rightItem signin_btn">{{ header_signin }}</div>
-        <div class="header_rightItem joinnow_btn">{{ header_joinnow }}</div>
+        <a href="https://www.starbucks.com/store-locator?map=39.635307,-101.337891,5z">
+        <div class="header_rightItem">{{ header_findastore }}</div></a>
+        <a href="https://www.starbucks.com/account/signin?ReturnUrl=%2F">
+        <div class="header_rightItem signin_btn">{{ header_signin }}</div></a>
+        <a href="https://www.starbucks.com/account/create">
+        <div class="header_rightItem joinnow_btn">{{ header_joinnow }}</div></a>
         </div>
     </div>
 </template>
